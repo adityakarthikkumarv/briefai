efecfafef40afaab31058d724cf44f6fa36c9c52
Aditya Karthik Kumar Vummadisingu <85030107+adityakarthikkumarv@users.noreply.github.com>
2026-06-07 20:07:54 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist'
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
   }
 })
